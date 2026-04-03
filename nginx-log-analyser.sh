490224a6b573c611292f7876c7af929469a53b53
Brian Gelhorn <briangelhorn@gmail.com>
2026-04-03 00:03:01 -0300

    Added top 5 received response status codes

--- nginx-log-analyser.sh
+++ nginx-log-analyser.sh
@@ -27,3 +27,14 @@ for ((i=1; i<=5; i++)); do
 	REQUESTS="$(echo "$TOPREQUESTS" | awk -v i="$i" ' NR==i { print $1 }')"
 	printf "%-30s %s\n" "$IP" "$REQUESTS"
 done
+echo
+
+TOPSTATUSCODES="$(awk -F '"' '{ print $3 }' "$FILEDIR" | awk '{ print $1 }' | sort -r | uniq -c | sort -gr | head -n 5)"
+echo "The top 5 most received response status codes:"
+printf "%-15s %s\n" "STATUS CODE" "TIMES RECEIVED"
+
+for ((i=1; i<=5; i++)); do
+	CODE="$(echo "$TOPSTATUSCODES" | awk -v i="$i" ' NR==i { print $2 }')"
+	TIMESRECEIVED="$(echo "$TOPSTATUSCODES" | awk -v i="$i" ' NR==i { print $1 }')"
+	printf "%-15s %s\n" "$CODE" "$TIMESRECEIVED"
+done
